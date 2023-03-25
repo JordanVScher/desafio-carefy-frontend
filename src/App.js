0221a36a18301ac7aaab2f590abb58412a435dc8
Jordan Victor <jordan.scher@cobasi.com.br>
2023-03-25 16:56:56 -0300

    feat: add Container and Title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import { toast, ToastContainer } from 'react-toastify';
+import styled from 'styled-components';
 import GlobalStyle from './styles/global';
 import 'react-toastify/dist/ReactToastify.css';
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2``;
 
 function App() {
   return (
     <>
+      <Container>
+        <Title>PACIENTES</Title>
+      </Container>
       <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
       <GlobalStyle />
     </>
