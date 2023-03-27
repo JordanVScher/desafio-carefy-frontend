db76d74b6b34cc874220c7acde0c9b16fc92e883
Jordan Victor <jordan.scher@cobasi.com.br>
2023-03-27 17:44:38 -0300

    feat: send authentication bearer token on patient requests

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,23 +5,27 @@ const baseUrl = BACKEND_URL;
 const patientUrl = `${baseUrl}/patient`;
 const authUrl = `${baseUrl}/auth`;
 
+const headers = {
+  Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+};
+
 export const getPatients = async (page, limit) => {
-  const { data } = await axios.get(`${patientUrl}?page=${page}&limit=${limit}`);
+  const { data } = await axios.get(`${patientUrl}?page=${page}&limit=${limit}`, { headers });
   return data;
 };
 
 export const deletePatient = async (id) => {
-  const { data } = await axios.delete(`${patientUrl}/${id}`);
+  const { data } = await axios.delete(`${patientUrl}/${id}`, { headers });
   return data;
 };
 
 export const updatePatient = async (id, newData) => {
-  const { data } = await axios.patch(`${patientUrl}/${id}`, newData);
+  const { data } = await axios.patch(`${patientUrl}/${id}`, newData, { headers });
   return data;
 };
 
 export const createPatient = async (newData) => {
-  const { data } = await axios.post(`${patientUrl}`, newData);
+  const { data } = await axios.post(`${patientUrl}`, newData, { headers });
   return data;
 };
 
