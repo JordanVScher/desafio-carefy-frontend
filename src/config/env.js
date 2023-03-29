be2c04743cffe2c444f3fcff41bce31ae9fea2e3
Jordan Victor <jordan.scher@cobasi.com.br>
2023-03-29 01:33:36 -0300

    chore: remove unnecessary envs

--- src/config/env.js
+++ src/config/env.js
@@ -2,6 +2,3 @@ export const ENV = process.env.REACT_APP_ENV ?? 'dev';
 export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:3500';
 
 export const GITHUB_OAUTH_CLIENT_ID = process.env.REACT_APP_GITHUB_OAUTH_CLIENT_ID;
-export const GITHUB_OAUTH_REDIRECT_URI = process.env.REACT_APP_GITHUB_OAUTH_REDIRECT_URI;
-export const GITHUB_OAUTH_CLIENT_SECRET = process.env.REACT_APP_GITHUB_OAUTH_CLIENT_SECRET;
-export const GITHUB_OAUTH_PROXY_URL = process.env.REACT_APP_GITHUB_OAUTH_PROXY_URL;
